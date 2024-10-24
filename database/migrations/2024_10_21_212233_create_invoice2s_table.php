5463936741f085a81ee05afa57a21c248af17b5f
AmmarElhenawy <ammarwabs@gmail.com>
2024-10-24 09:00:12 -0700

    main invoices files

---
+++ database/migrations/2024_10_21_212233_create_invoice2s_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice2s', function (Blueprint $table) {
+            $table->id();
+            $table->string('user');
+            $table->string('invoice_num');
+            $table->string('rte_vat');
+            $table->string('status');
+            $table->string('section');
+            $table->string('discount');
+            $table->integer('value_status');//for search
+            $table->decimal('value_vat');
+            $table->decimal('total');
+            $table->date('invoice_date');
+            $table->date('due_date');
+            $table->text('note')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice2s');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Invoice2Controller;
+// use App\Http\Controllers\AdminController;
+// use App\Http\Controllers\HomeController;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('invoices', Invoice2Controller::class);
+Route::get('/{page}', 'App\Http\Controllers\AdminController@index');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
